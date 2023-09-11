576a256d0cfe047034343188b1d859c3661ac45e
tlippert <t.lippert@str8labs.com>
2023-09-11 02:07:28 +0200

    label circle width hack

--- src/app/components/check/check-label/check-label.component.ts
+++ src/app/components/check/check-label/check-label.component.ts
@@ -8,4 +8,10 @@ import { CheckBaseComponent } from '../check-base/check-base.component';
 })
 export class CheckLabelComponent extends CheckBaseComponent {
 
+  public get width(): number {
+    // Hack: Calculate circle image width depending on char count
+    // Clamp between 50 and 100% for 4 to 9 chars
+    const chars = this.label?.length ?? 0;
+    return Math.min(Math.max(chars, 4), 9) * 10 + 10;
+  }
 }
