eac835fb60aa9591d3c0b72893d9c0e26d0073e5
tlippert <t.lippert@str8labs.com>
2023-09-13 16:27:58 +0200

    disable impossible abilities

--- src/app/components/abilities-card/abilities-card.component.ts
+++ src/app/components/abilities-card/abilities-card.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Tags } from 'src/app/services/ghosts';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Ghosts, Tags } from 'src/app/services/ghosts';
 import { GhostFilters, GhostName, Tag, TagData } from 'src/app/services/models';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { CheckState } from '../check/check-base/check-base.component';
@@ -16,19 +16,32 @@ import { CheckState } from '../check/check-base/check-base.component';
     ])
   ]
 })
-export class AbilitiesCardComponent {
+export class AbilitiesCardComponent implements OnChanges {
   @Input() public filters = new GhostFilters();
   @Output() public filtersChange = new EventEmitter<GhostFilters>();
 
   @Input() public showTips: boolean = false;
   @Input() public ghostsDisabled = new Set<GhostName>();
 
+  public readonly CheckState = CheckState;
+
   public readonly tags: TagData[] = Object.values(Tags);
+  public enabledTags: Set<string> = new Set<string>();
 
   public get tipTags(): TagData[] {
     return this.tags
       .filter(t => t.description != null)
       .filter(t => this.filters.tagsSelected.has(t.key) || this.filters.tagsEliminated.has(t.key));
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['ghostsDisabled']) {
+      const tags = new Set<Tag>();
+      Object.values(Ghosts)
+        .filter(g => !this.ghostsDisabled.has(g.name))
+        .forEach(g => g.tags.forEach(t => tags.add(t)));
+      this.enabledTags = tags;
+    }
   }
 
   public getState(tag: TagData): CheckState {
